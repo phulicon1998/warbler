342ae92e5c3b87df509b833ad562c4431fe28c01
phulicon1998 <phulicon815490@gmail.com>
2018-12-14 23:27:07 +0700

    Finish backend with some functionalities

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -5,24 +5,35 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 const authRoutes = require("./routes/auth");
-const db = require("./models");
-
-const PORT = 8081;
+const messagesRoutes = require("./routes/messages");
+const {loginRequired, ensureCorrectUser} = require("./middleware/auth");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users/:id/messages", loginRequired, ensureCorrectUser, messagesRoutes);
+app.use("/api/messages", loginRequired, async function(req, res, next){
+    try{
+        let messages = db.Message.find().sort({createdAt: "desc"}).populate("user", {
+            username: true,
+            profileImageUrl: true
+        })
+        return res.status(200).json(messages);
+    }catch(err){
+        return next(err);
+    }
+})
 
 app.use(function(req, res, next){
-    let err = new Error("Not Found!");
+    let err = new Error("Route Not Found!");
     err.status = 404;
     next(err);
 })
 
 app.use(errorHandler);
 
-app.listen(PORT, function(){
-    console.log(`Server starts on port ${PORT}`)
+app.listen(process.env.PORT, function(){
+    console.log(`Server starts on port ${process.env.PORT}`)
 });
