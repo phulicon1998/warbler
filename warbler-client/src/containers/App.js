e789181a2dd54b5a69c4b733877d88c150608a13
phulicon1998 <phulicon815490@gmail.com>
2018-12-15 14:38:55 +0700

    Setup for frontend

---
+++ warbler-client/src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Provider} from "react-redux";
+import {configureStore} from "../store";
+import {BrowserRouter as Router} from "react-router-dom";
+
+const store = configureStore();
+
+const App = () => (
+    <Provider store={store}>
+         <Router><div>Hello world</div></Router>
+    </Provider>
+)
+
+export default App;
---
+++ warbler-client/src/store/reducers/index.js
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+import currentUser from "./currentUser";
+import errors from "./error";
+
+const rootReducer = combineReducers({ currentUser, errors });
+
+export default rootReducer;
