ee5c3e358876608736ba1db99741af9a339035f5
phulicon1998 <phulicon815490@gmail.com>
2018-12-18 18:10:18 +0700

    Prepare for Deploy

--- warbler-server/models/index.js
+++ warbler-server/models/index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/warbler", { keepAlive: true, useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/warbler", { keepAlive: true, useNewUrlParser: true });
+mongoose.connect(process.env.WARBLER_DB_URL, { useNewUrlParser: true });
+// mongodb://kyle:kyle123@ds125372.mlab.com:25372/ky-warbler
 
 module.exports.User = require("./user");
 module.exports.Message = require("./message");
